b3e35b11f66aa116db477a99652531203d753ff9
Alexey Mogilyovkin <zeroicq@gmail.com>
2019-09-24 13:19:35 +1000

    Tired of long math implementation

--- RSA/LargeInt.cs
+++ RSA/LargeInt.cs
@@ -6,23 +6,38 @@ namespace RSA {
 public class LargeUInt {
     // the lower index the higher the position
     public byte[] buf;
+    public bool isEven => (buf.Last() & 1) != 1;
     
-    public LargeUInt(uint bytesLength) {
+    public LargeUInt(int bytesLength) {
         buf = new byte[bytesLength];
     }
+    
 
     public LargeUInt(LargeUInt other) {
         buf = new byte[other.buf.Length];
         Array.Copy(other.buf, buf, buf.Length);
     }
 
+    public static LargeUInt FromInt(int i) {
+        var res = new LargeUInt(sizeof(int));
+
+        for (var j = 0; j < sizeof(int); j++) {
+            res.buf[res.buf.Length -  1 - j]  = (byte)(i >> (j*8) & 0xff); 
+        }
+//        var lolo = (byte)(i & 0xff);
+//        var hilo = (byte)((i >> 8) & 0xff);
+//        var lohi = (byte)((i >> 16) & 0xff);
+//        var hihi = (byte)(i >> 24);
+        return res;
+    }
+
     public static LargeUInt operator -(LargeUInt lhs, byte rhs) {
         var result = new LargeUInt(lhs);
-        var minByte = result.buf[result.buf.Length - 1];
+        var minByteVal = result.buf[result.buf.Length - 1];
 
-        if (minByte < rhs) {
+        if (minByteVal < rhs) {
             // borrow
-            int tmp = Byte.MaxValue + minByte;
+            int tmp = Byte.MaxValue + 1 + minByteVal;
             result.buf[result.buf.Length - 1] = (byte)(tmp - rhs);
 
             int curIndex = result.buf.Length - 2;
@@ -45,6 +60,31 @@ public class LargeUInt {
 
         return result;
     }
+    
+    public static LargeUInt DivideBy2(LargeUInt number) {
+        var result = new LargeUInt(number.buf.Length);
+        var curIndex = result.buf.Length - 1;
+        
+        while (curIndex >= 0) {
+            result.buf[curIndex] = (byte)(number.buf[curIndex] >> 1);
+            if (curIndex == 0)
+                break;
+
+            var higherBit = (byte)(number.buf[curIndex - 1] & 1);
+            higherBit = (byte) (higherBit << 7);
+            result.buf[curIndex] &= 127;
+            result.buf[curIndex] |= higherBit;
+            curIndex--;
+        }
+
+        return result;
+    }
+
+    public void IncreaseLengthTo(int bufLength) {
+        var newBuf = new byte[bufLength];
+        Array.Copy(buf, 0, newBuf, buf.Length - 1, buf.Length);
+        buf = newBuf;
+    }
 
     public string ToString() {
         var strBld = new StringBuilder();
@@ -58,6 +98,5 @@ public class LargeUInt {
         }
         return strBld.ToString();
     }
-    
 }
 }
--- RSA/Rsa.cs
+++ RSA/Rsa.cs
@@ -8,15 +8,18 @@ class Program {
     private static RNGCryptoServiceProvider rngCsp = new RNGCryptoServiceProvider();
     
     static void Main(string[] args) {
-        Console.WriteLine("Hello World!");
-        var test = GenerateRandomNumber(1024/8);
-        Console.WriteLine(test.ToString());
-        Console.WriteLine((test - 1).ToString());
+        var test = GenerateRandomNumber(2);
+        BigInteger a = new BigInteger();
+        test.buf[0] = 45;
+        test.buf[1] = 191;
+        Console.WriteLine(TestMillerRabin(test, 2));
         rngCsp.Dispose();
     }
 
-    static LargeUInt GenerateRandomNumber(uint bytesLength) {
+    static LargeUInt GenerateRandomNumber(int bytesLength) {
         var number = new LargeUInt(bytesLength);
+        // make odd
+        number.buf[number.buf.Length - 1] |= 1;
         rngCsp.GetBytes(number.buf);
         return number;
     }
@@ -29,9 +32,58 @@ class Program {
 //        }
 //    }
 
-//    static bool TestMillerRabin(LargeUInt n, int rounds) {
-//        LargeUInt nMinusOne = n - 1;
-//    }
+    static bool TestMillerRabin(LargeUInt n, int rounds) {
+        LargeUInt nMinusOne = n - 1;
+        
+        // n-1=2^s *t
+        var s = 0;
+        var t = new LargeUInt(nMinusOne);
+        while (t.isEven) {
+            t = LargeUInt.DivideBy2(t);
+            s++;
+        }
 
+        for (int i = 0; i < rounds; i++) {
+            //Выбрать случайное целое число a в отрезке [2, n − 2]
+            var a = GenRndLUint(LargeUInt.FromInt(2), n - 2);
+            var x = a pow t mod n;
+        }
+
+//        return true;
+    }
+    
+    // [min, max]
+    static LargeUInt GenRndLUint(LargeUInt min, LargeUInt max) {
+        var res = new LargeUInt(max.buf.Length);
+        
+        if (min.buf.Length < max.buf.Length)
+            min.IncreaseLengthTo(max.buf.Length);
+        
+        var isLessMax = false;
+        var isMoreMin = false;
+        var rnd = new Random();
+        
+        for (var i = 0; i < max.buf.Length; i++) {
+            var upperBound = Byte.MaxValue + 1;
+            if (!isLessMax)
+                upperBound = max.buf[i] + 1;
+
+            var lowerBound = Byte.MinValue;
+            if (!isMoreMin)
+                lowerBound = min.buf[i];
+            
+            var bt = (byte)rnd.Next(lowerBound, upperBound);
+            
+            if (bt < max.buf[i])
+                isLessMax = true;
+
+            if (bt > min.buf[i])
+                isMoreMin = true;
+
+            res.buf[i] = bt;
+        }
+
+        return res;
+    }
 }
 }
